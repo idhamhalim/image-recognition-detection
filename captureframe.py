5287a030ffcc6443f81b8ff51f1e9eb04d30cb40
Idham Halim <iddy93@gmail.com>
2018-03-27 11:12:27 +0800

    Add files via upload
    captureframe.py is to capture feed from rtsp feed or any links
    predict-idham.py is to check whether captured frames has car or not

---
+++ captureframe.py
@@ -0,0 +1,54 @@
+#import dependencies
+import os
+from darkflow.net.build import TFNet
+from os import listdir
+from os.path import isfile, join
+import numpy
+import cv2
+import time
+
+timestr = time.strftime("%Y%m%d%H%M%S")
+
+#for automatic frame capture, prints success for every frame recorded
+#get frames from live rtsp feed , 3 frames per second, url can be configured
+vidcap = cv2.VideoCapture("rtsp://admin:admin123@tapway1.dlinkddns.com:98/cam/realmonitor?channel=1&subtype=0");
+success,image = vidcap.read()
+count = 0
+success = True
+mypath = '/home/tapway-office/darkflow-master/frames-idham'
+while success:
+    success,image = vidcap.read()
+    print('read a new frame:',success)
+    if count%7 == 0 :
+
+         cv2.imwrite(os.path.join(mypath ,'framee%s.jpg'%timestr),image)
+         #saving images to mypath
+         #cv2.imwrite(os.path.join(mypath ,'frames%d.jpg'%count,image)
+         print('success')
+    count+=1
+
+#setting path of folder with input images,
+#can be configured for your own path/folder
+#mypath='/home/idham/darkflow-master/image/'
+
+#loading multiple frames into tiny-yolo-voc model, model can be configured
+# options = {"model": "cfg/tiny-yolo-voc.cfg",
+#             "load": "tiny-yolo-voc.weights",
+#             "threshold": 0.1}
+#
+# tfnet = TFNet(options)
+#
+# onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
+# images = numpy.empty(len(onlyfiles), dtype=object)
+# for n in range(0, len(onlyfiles)):
+#     count = 0
+#     images[n] = cv2.imread( join(mypath,onlyfiles[n]) )
+#     result = tfnet.return_predict(images[n])
+#     for r in result:
+#
+#         if (r['label']) == 'car':
+#             cv2.imwrite('aimg%d.jpg'%count, images[n])
+#         else:
+#                 print('')
+#
+#         count+=1
---
+++ predict-idham.py
@@ -0,0 +1,47 @@
+import os
+from darkflow.net.build import TFNet
+from os import listdir
+from os.path import isfile, join
+import numpy
+import cv2
+import shutil
+
+#setting path of folder with input images,
+#can be configured for your own path/folder
+mypath='/home/tapway-office/darkflow-master/frames-idham'
+source = os.listdir("/home/tapway-office/darkflow-master/")
+destination = "/home/tapway-office/darkflow-master/frames-car/"
+
+for files in source:
+    if files.endswith(".jpg"):
+        shutil.move(files,destination)
+
+#loading multiple frames into tiny-yolo-voc model, model can be configured
+options = {"model": "cfg/tiny-yolo-voc.cfg",
+            "load": "tiny-yolo-voc.weights",
+            "threshold": 0.1}
+
+tfnet = TFNet(options)
+
+onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
+images = numpy.empty(len(onlyfiles), dtype=object)
+success = True
+
+for n in range(0, len(onlyfiles)):
+
+    count = 5
+    images[n] = cv2.imread( join(mypath,onlyfiles[n]) )
+    result = tfnet.return_predict(images[n])
+    for r in result:
+
+        if (r['label']) == 'car':
+            cv2.imwrite('frame%d.jpg'%count, images[n])
+            for files in source:
+                if files.endswith(".jpg"):
+                    shutil.move(files, destination)
+
+        else:
+            cv2.imwrite('blank.jpg', images[n])
+
+
+        count+=1
